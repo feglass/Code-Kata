638137ddb50f0f4093ada407043cd9e75b1547f4
Sinno <feglass@msn.cn>
2016-04-28 11:31:00 +0800

    Added files via upload

---
+++ CodeForces/4A_1.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+//3920K, 1500Ms, 1352B
+public class Main {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int w = sc.nextInt();
+		int i, flag;
+		
+        if(w%2==0&& w>2)
+        	System.out.println("YES");    
+        else 
+        	System.out.println("NO");
+	
+	}
+	
+}
